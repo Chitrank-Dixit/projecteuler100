50509631c722ade24a1da9e829a8f80df0aa9736
Chitrank Dixit <chitrankdixit@gmail.com>
2020-01-11 23:46:32 +0530

    add day6 program for challenge

---
+++ day6.py
@@ -0,0 +1,19 @@
+from itertools import compress
+
+
+def sieve_of_eratosthenes(n):
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while (p * p) <= n:
+        if prime[p]:
+            for i in range(p * 2, n + 1, p):
+                prime[i] = False
+        p += 1
+    prime[0] = False
+    prime[1] = False
+    return list(compress(range(len(prime)), prime))
+
+
+dictio = sieve_of_eratosthenes(1000000)
+print(dictio[10001 - 1])
+
